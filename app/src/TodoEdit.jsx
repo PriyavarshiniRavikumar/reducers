b063fb62aa5244c3d6657562b94cd93f369731b0
PriyavarshiniRavikumar <146017094+PriyavarshiniRavikumar@users.noreply.github.com>
2023-11-18 15:02:38 +0530

    Update TodoEdit.jsx onsave

--- app/src/TodoEdit.jsx
+++ app/src/TodoEdit.jsx
@@ -1,1 +1,7 @@
 import {useState} from "react";
+function TodoEdit({ todo, onSave }) {
+  const [editText, seteditText] = useState(todo.text);
+  function handleEditSubmit(e) {
+    e.preventDefault();
+    onSave(editText);
+  }
